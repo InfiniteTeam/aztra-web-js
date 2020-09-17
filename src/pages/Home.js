427acb170ea8cea466b85e8d7383026cfbb0426e
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-09-17 20:56:06 +0900

    Add: 홈화면 버튼과 로고 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
+import logoinv from '../aztra_inv.png'
 
 export default class Home extends Component {
   render() {
@@ -8,17 +9,19 @@ export default class Home extends Component {
         <Container fluid>
           <Container fluid="sm" className="ct Intro-ct">
             <div className="Main-intro">
-              <p>
-                <h1 className="no-drag text-white text-center">
-                  미래를 바꿀 디스코드 관리봇, Aztra
-                </h1>
-              </p>
-              <p>
-                <h2 className="no-drag text-white text-center">
-                  다채롭고 깔끔한 디스코드 서버를 만들 수 있도록 도와드리겠습니다.
-                </h2>
-              </p>
-
+              <img src={logoinv} className="logo pb-3 img-view-only no-drag" alt="Aztra" />
+              <h1 className="no-drag text-white">
+                미래를 바꿀 디스코드 관리봇, Aztra
+              </h1>
+              <h2 className="no-drag text-white pb-5">
+                다채롭고 깔끔한 디스코드 서버를 만들 수 있도록 도와드리겠습니다.
+              </h2>
+              <Button variant="aztra" size="lg" className="Main-button">
+                초대하기
+              </Button>
+              <Button variant="light" size="lg" className="Main-button">
+                자세히 알아보기
+              </Button>
             </div>
           </Container>
         </Container>
