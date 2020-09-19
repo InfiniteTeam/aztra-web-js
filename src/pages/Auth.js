d322d26320bfaee9871905e298c4093a6499b1c4
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-09-19 22:36:28 +0900

    Add: 로그인 후 리다이렉션

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
 export default class Auth extends Component {
-    componentDidMount() {
-        var code = new URLSearchParams(this.props.location.search).get('code')
-        localStorage.setItem('token', code)
-    }
+  componentDidMount() {
+    var code = new URLSearchParams(this.props.location.search).get('code')
+    localStorage.setItem('token', code)
+  }
 
-    render() {
-        return null
-    }
+  render() {
+    return (
+      <div>
+        로그인 중
+        <Redirect to="/" />
+      </div>
+    )
+  }
 }
